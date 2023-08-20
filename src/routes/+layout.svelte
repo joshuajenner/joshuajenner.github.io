cd8ee1769bbbf201b22106609504faeefbd09307
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2023-08-21 00:19:20 +0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <nav>
 	<ul>
-		<li><a href="/" id="nav_title">JJ<span>D</span></a></li>
+		<li><a href="#" id="nav_title">JJ<span>D</span></a></li>
 		<div id="socials">
 			<li><a href="https://www.youtube.com/channel/UCyizGS2XtfLldHkQwF0NC1w"><img alt="" class="dark" src="../icons/ytd.png" /><img alt="" class="light" src="../icons/ytl.png" /></a></li>
 			<li><a href="https://twitter.com/joshuajennerdev"><img alt="" class="dark" src="../icons/twd.png" /><img alt="" class="light" src="../icons/twl.png" /></a></li>
