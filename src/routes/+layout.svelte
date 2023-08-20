0647b52edd7536dc2a44910faa2de7f6c5397337
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2023-08-21 00:22:31 +0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
+<script>
+	import { base } from "$app/paths";
+</script>
+
 <nav>
 	<ul>
-		<li><a href="#" id="nav_title">JJ<span>D</span></a></li>
+		<li><a href={base} id="nav_title">JJ<span>D</span></a></li>
 		<div id="socials">
-			<li><a href="https://www.youtube.com/channel/UCyizGS2XtfLldHkQwF0NC1w"><img alt="" class="dark" src="../icons/ytd.png" /><img alt="" class="light" src="../icons/ytl.png" /></a></li>
-			<li><a href="https://twitter.com/joshuajennerdev"><img alt="" class="dark" src="../icons/twd.png" /><img alt="" class="light" src="../icons/twl.png" /></a></li>
-			<li><a href="https://www.twitch.tv/joshuajennerdev"><img alt="" class="dark" src="../icons/ttvd.png" /><img alt="" class="light" src="../icons/ttvl.png" /></a></li>
+			<li><a href="https://www.youtube.com/channel/UCyizGS2XtfLldHkQwF0NC1w"><img alt="" class="dark" src="%sveltekit.assets%/icons/ytd.png" /><img alt="" class="light" src="%sveltekit.assets%/icons/ytl.png" /></a></li>
+			<li><a href="https://twitter.com/joshuajennerdev"><img alt="" class="dark" src="%sveltekit.assets%/icons/twd.png" /><img alt="" class="light" src="%sveltekit.assets%/icons/twl.png" /></a></li>
+			<li><a href="https://www.twitch.tv/joshuajennerdev"><img alt="" class="dark" src="%sveltekit.assets%/icons/ttvd.png" /><img alt="" class="light" src="%sveltekit.assets%/icons/ttvl.png" /></a></li>
 		</div>
 	</ul>
 </nav>
