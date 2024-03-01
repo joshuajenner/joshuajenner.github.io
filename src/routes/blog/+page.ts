5ce52c73de8f1ef070e7bef052c3053db9c6be7b
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2024-03-01 12:24:53 +0800

    Removed Unused Code & Assets

--- src/routes/blog/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PostDisplay } from '$lib/types'
-
-export async function load({ fetch }) {
-    const response = await fetch('blog/api')
-    const posts: PostDisplay[] = await response.json()
-    return { posts }
-}
